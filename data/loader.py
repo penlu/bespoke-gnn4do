b538ed70f264293d5f844ef05b7cb821aa53e83b
Eric Lu <penlume@gmail.com>
2023-08-16 12:43:10 -0400

    debug RandomGraphDataset

--- data/loader.py
+++ data/loader.py
@@ -1,19 +1,24 @@
-# Dataset loading
+# Dataset loading functionality
 
 import torch
-from torch.geometric.data import Dataset
+import networkx
+import numpy
+from torch_geometric.data import InMemoryDataset
 from torch_geometric.datasets import TUDataset
+from torch_geometric.utils.convert import from_networkx
 from utils.graph_utils import gen_graph
 
 # TODO check if this works right
-class RandomGraphDataset(Dataset):
+class RandomGraphDataset(InMemoryDataset):
     def __init__(self, root,
                   num_graphs=10000, num_nodes_per_graph=100, edge_probability=0.15,
+                  seed=0,
                   transform=None, pre_transform=None, pre_filter=None):
-        super().__init__(root, transform, pre_transform, pre_filter)
         self.num_graphs = num_graphs
         self.num_nodes_per_graph = num_nodes_per_graph
         self.edge_probability = edge_probability
+        self.seed = seed
+        super(RandomGraphDataset, self).__init__(root, transform, pre_transform, pre_filter)
         self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
@@ -23,17 +28,18 @@ class RandomGraphDataset(Dataset):
 
     @property
     def processed_file_names(self):
-        return [f'random_{self.num_graphs}_{self.num_nodes_per_graph}_{self.edge_probability}.pt']
+        return [f'random_{self.num_graphs}_{self.num_nodes_per_graph}_{self.edge_probability}_{self.seed}.pt']
 
     def download(self):
         # no download needed
         pass
 
     def process(self):
-        # create random graphs and save
+        # create random undirected graphs and save
+        random_state = numpy.random.RandomState(seed)
         data_list = []
         for i in range(self.num_graphs):
-            G = nx.erdos_renyi_graph(self.num_nodes_per_graph, self.edge_probability)
+            G = networkx.erdos_renyi_graph(self.num_nodes_per_graph, self.edge_probability, seed=random_state)
             data_list.append(from_networkx(G))
 
         if self.pre_filter is not None:
