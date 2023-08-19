0b12edbeec4ee20e55efb9832b4c8e021ac53ce1
JessJKitty <jx.silvershine@gmail.com>
2023-08-19 11:31:56 -0400

    test working

--- test.py
+++ test.py
@@ -10,9 +10,11 @@ from model.training import predict
 from model.models import construct_model
 from model.losses import get_loss_fn
 from model.saving import load_model
+import pickle
+from datetime import datetime
 
 '''
-python test.py --model_folder="/home/bcjexu/maxcut-80/bespoke-gnn4do/training_runs/2023-08-18_13:15:25" \
+python test.py --model_folder="/home/bcjexu/maxcut-80/bespoke-gnn4do/training_runs/_2023-08-19_09:22:09" \
     --model_file=model_ep0.pt
 '''
 
@@ -29,6 +31,10 @@ if __name__ == '__main__':
 
     # call test model
     predictions = predict(model, test_loader, args)
-    # TODO: save predictions
-    print("hi")
+
+    # TODO: fix output file?
+    pickle.dump(model, open(os.path.join(args.model_folder, 
+                                        f'test_results_{datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}.pkl'), "wb"))
+
+    print("finished predicting!")
     
