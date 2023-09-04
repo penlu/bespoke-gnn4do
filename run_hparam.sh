d71845be70fac3228b8589bd8691bdccb771fa1d
JessJKitty <jx.silvershine@gmail.com>
2023-09-04 15:37:54 -0400

    changed penalties to not be 0

--- run_hparam.sh
+++ run_hparam.sh
@@ -15,17 +15,11 @@ run_job () {
     else
         TYPE='TU'
     fi
-    smallrs=('2' '4' '8')
-    if [[ ${smallrs[@]} =~ $R ]] ;then
-        PE_DIM=$R
-    else
-        PE_DIM='8'
-    fi
     echo $MODEL $TYPE $DATASET $((SLOT - 1))
     CUDA_VISIBLE_DEVICES=$((SLOT - 1)) python train.py \
         --stepwise=True --steps=50000 \
         --valid_freq=100 --dropout=0 \
-        --prefix=230904_hparam_att1 \
+        --prefix=230904_hparam_test \
         --model_type=$MODEL --TUdataset_name=$DATASET --dataset=$TYPE \
         --num_layers=$LIFT_LAYERS --rank=$R --vc_penalty=$PENALTY --problem_type=vertex_cover
 }
@@ -33,9 +27,9 @@ export -f run_job
 
 for model in 'LiftMP' ; do
     for dataset in 'RANDOM' ; do
-        for penalty in '0' '1' '2' '3' '4' '5'; do
-            for r in '2' '4' '8' '16' '32'; do
-                for lift_layers in '1' '2' '3' '4' '6' '10'; do
+        for penalty in '0.1' '0.25' '0.5' '1' '2' '4' ; do
+            for r in '2' '4' '8' '16' '32' ; do
+                for lift_layers in '1' '2' '3' '4' '6' '10' ; do
                     echo $model $dataset $penalty $r $lift_layers
                 done
             done
