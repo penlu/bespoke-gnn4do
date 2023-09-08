3ad808489f5d9e105686236c67e1cdb1084e8aef
Eric Lu <penlume@gmail.com>
2023-09-08 14:47:30 -0400

    add downloader script

---
+++ download.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+import torch
+
+from data.loader import construct_dataset
+
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+# construct random
+construct_dataset(dotdict({
+    'dataset': 'RANDOM',
+    'num_graphs': 1000,
+    'num_nodes_per_graph': 100,
+    'edge_probability': 0.15
+}))
+
+# construct many TU datasets
+for TU_name in ['ENZYMES', 'PROTEINS', 'IMDB-BINARY', 'MUTAG', 'COLLAB']:
+    construct_dataset(dotdict({'dataset': 'TU', 'TUdataset_name': TU_name}))
