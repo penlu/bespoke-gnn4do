8065d5fee8f0819018cdb316c892f165b9f86bfe
Eric Lu <penlume@gmail.com>
2023-09-16 14:22:39 +0000

    script for running fat gurobi

--- run-baselines.sh
+++ run-baselines.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
-for i in RANDOM ENZYMES PROTEINS IMDB-BINARY MUTAG COLLAB; do
-    echo $i
-    if [ $i = 'RANDOM' ] ; then
+if [ "$#" -ne 1 ]; then
+    echo "Please invoke with exactly one parameter: problem_type!"
+fi
+
+if [[ $1 != 'max_cut' && $1 != 'vertex_cover' ]] ; then
+    echo "Invalid problem_type $1"
+    exit
+fi
+
+for DATASET in RANDOM ENZYMES PROTEINS IMDB-BINARY MUTAG COLLAB ForcedRB; do
+    echo $DATASET
+    if [ $DATASET = 'RANDOM' ] ; then
         TYPE='RANDOM'
+    elif [ $DATASET = 'ForcedRB' ] ; then
+        TYPE='ForcedRB'
     else
         TYPE='TU'
     fi
     python baselines.py --dataset $TYPE \
-      --problem_type vertex_cover \
-      --prefix 230902_gurobi_5s_$i --TUdataset_name $i \
-      --gurobi True --gurobi_timeout 5
+      --problem_type $1 \
+      --prefix 230916_gurobi_20s_$DATASET --TUdataset_name $DATASET \
+      --gurobi True --gurobi_timeout 20
 done
