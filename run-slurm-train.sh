8d4192a2864ac283919c152b27e6cc5bf3e1fd5a
Eric Lu <penlume@gmail.com>
2023-09-25 12:16:21 -0400

    remove redundant run scripts

--- run-slurm-train.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-for model in 'LiftMP' 'GIN' 'GAT' 'GCNN' 'GatedGCNN' 'NegationGAT' ; do
-    for dataset in 'RANDOM' 'ForcedRB' 'ENZYMES' 'PROTEINS' 'IMDB-BINARY' 'MUTAG' 'COLLAB' ; do
-        LLsub ./submit-train.sh -- $MODEL $DATASET
-    done
-done
