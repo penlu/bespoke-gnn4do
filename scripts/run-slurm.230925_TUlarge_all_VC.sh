a1c4ad231df47f65cfc8f496fb4056cc8131ce7f
Eric Lu <penlume@gmail.com>
2023-09-25 18:14:09 -0400

    add 230925_TUlarge_all_VC script

---
+++ scripts/run-slurm.230925_TUlarge_all_VC.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+export PREFIX=230925_TUlarge_all_VC
+
+for MODEL in 'LiftMP' 'GIN' 'GAT' 'GCNN' 'GatedGCNN' ; do
+    for DATASET in 'REDDIT-MULTI-5K' 'REDDIT-MULTI-12K' 'REDDIT-BINARY'; do
+        for LIFT_LAYERS in '8' '16' ; do
+            for R in '4' '8' '16' '32' ; do
+                echo $MODEL $DATASET $R $LIFT_LAYERS
+                LLsub ./submit.sh -- \
+                  --stepwise=True --steps=100000 \
+                  --valid_freq=1000 --dropout=0 \
+                  --prefix=$PREFIX \
+                  --model_type=$MODEL --dataset=$DATASET \
+                  --num_layers=$LIFT_LAYERS --rank=$R \
+                  --vc_penalty=1 --problem_type=vertex_cover \
+                  --batch_size=16 --positional_encoding=random_walk --pe_dimension=$((R/2))
+            done
+        done
+    done
+done
