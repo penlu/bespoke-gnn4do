3e2abc3d9f8daa11f55887111bb5110ba6e4ab2b
Eric Lu <penlume@gmail.com>
2023-09-25 23:50:12 -0400

    add 230925_gurobi_TU_VC script

---
+++ scripts/run-slurm.230925_gurobi_TU_VC.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+export PREFIX=230925_gurobi_TU_VC
+
+for DATASET in 'ENZYMES' 'PROTEINS' 'MUTAG' 'IMDB-BINARY' 'COLLAB' 'REDDIT-MULTI-5K' 'REDDIT-MULTI-12K' 'REDDIT-BINARY'; do
+    for TIMEOUT in 2 4 8 ; do
+        echo $DATASET $TIMEOUT
+        LLsub ./submit-baseline.sh -- \
+          --dataset=$DATASET --problem_type=vertex_cover \
+          --prefix=$PREFIX --gurobi=True --gurobi_timeout=$TIMEOUT
+    done
+done
