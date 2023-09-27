436205984a018dff9f6707178a702079b7c9af98
Eric Lu <penlume@gmail.com>
2023-09-27 16:40:29 -0400

    only run TUlarge on liftMP

--- scripts/run-slurm.230925_TUlarge_liftMP_VC.sh
+++ scripts/run-slurm.230925_TUlarge_liftMP_VC.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-export PREFIX=230925_TUlarge_all_cut
+export PREFIX=230925_TUlarge_liftMP_VC
 
-for MODEL in 'LiftMP' 'GIN' 'GAT' 'GCNN' 'GatedGCNN' ; do
+for MODEL in 'LiftMP' ; do
     for DATASET in 'REDDIT-BINARY' 'REDDIT-MULTI-5K' 'REDDIT-MULTI-12K' ; do
         # these fractions are tuned to produce a val and test split of 100 each
         if [ $DATASET = 'REDDIT-BINARY' ] ; then
@@ -24,17 +24,17 @@ for MODEL in 'LiftMP' 'GIN' 'GAT' 'GCNN' 'GatedGCNN' ; do
                   --prefix=$PREFIX \
                   --model_type=$MODEL --dataset=$DATASET --train_fraction=$FRACTION \
                   --num_layers=$LIFT_LAYERS --rank=$R \
-                  --problem_type=max_cut \
+                  --vc_penalty=1 --problem_type=vertex_cover \
                   --batch_size=16 --positional_encoding=random_walk --pe_dimension=$((R/2))
             done
-            echo $MODEL $DATASET $R $LIFT_LAYERS
+            echo $MODEL $DATASET 32 $LIFT_LAYERS
             LLsub ./submit.sh -- \
               --stepwise=True --steps=100000 \
               --valid_freq=2000 --dropout=0 \
               --prefix=$PREFIX \
               --model_type=$MODEL --dataset=$DATASET --train_fraction=$FRACTION \
               --num_layers=$LIFT_LAYERS --rank=32 \
-              --problem_type=max_cut \
+              --vc_penalty=1 --problem_type=vertex_cover \
               --batch_size=16 --positional_encoding=random_walk --pe_dimension=8
         done
     done
