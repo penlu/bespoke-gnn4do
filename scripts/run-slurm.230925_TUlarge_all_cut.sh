2c3a811718bd505c9b3caf6b6fda032faa61ac48
Eric Lu <penlume@gmail.com>
2023-09-26 23:56:32 -0400

    add 230925_TUlarge_all_cut script

---
+++ scripts/run-slurm.230925_TUlarge_all_cut.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+export PREFIX=230925_TUlarge_all_cut
+
+for MODEL in 'LiftMP' 'GIN' 'GAT' 'GCNN' 'GatedGCNN' ; do
+    for DATASET in 'REDDIT-MULTI-5K' 'REDDIT-MULTI-12K' 'REDDIT-BINARY'; do
+        for LIFT_LAYERS in '8' '16' ; do
+            for R in '4' '8' '16' ; do
+                echo $MODEL $DATASET $R $LIFT_LAYERS
+                LLsub ./submit.sh -- \
+                  --stepwise=True --steps=100000 \
+                  --valid_freq=1000 --dropout=0 \
+                  --prefix=$PREFIX \
+                  --model_type=$MODEL --dataset=$DATASET \
+                  --num_layers=$LIFT_LAYERS --rank=$R \
+                  --problem_type=max_cut \
+                  --batch_size=16 --positional_encoding=random_walk --pe_dimension=$((R/2))
+            done
+            echo $MODEL $DATASET $R $LIFT_LAYERS
+            LLsub ./submit.sh -- \
+              --stepwise=True --steps=100000 \
+              --valid_freq=1000 --dropout=0 \
+              --prefix=$PREFIX \
+              --model_type=$MODEL --dataset=$DATASET \
+              --num_layers=$LIFT_LAYERS --rank=32 \
+              --problem_type=max_cut \
+              --batch_size=16 --positional_encoding=random_walk --pe_dimension=8
+        done
+    done
+done
