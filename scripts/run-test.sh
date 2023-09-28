0eb3b129f8cfce7ba7f83705af12459ddbbd70ce
Eric Lu <penlume@gmail.com>
2023-09-28 19:02:00 -0400

    run-test is dangerous

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -5,5 +5,6 @@ set -e
 for i in $(cat analysis_ipynb/models_for_test.txt | grep -v "nan," | cut -d "'" -f 2) ; do
     if [ -d $i ] ; then
         echo $i
+        LLsub ./submit-test.sh -- --model_folder="$i" --model_file=best_model.pt --test_prefix=time_and_score
     fi
 done
