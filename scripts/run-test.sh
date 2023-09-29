1106a5d132fba3a32ae8d67e7a84143861190d12
Eric Lu <penlume@gmail.com>
2023-09-28 23:32:51 -0400

    do not run duplicate tests

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for i in $(cat analysis_ipynb/models_for_test.txt | grep -v "nan," | cut -d "'" -f 2) ; do
+for i in $(cat analysis_ipynb/models_for_test.txt | grep -v "nan," | cut -d "'" -f 2 | sort | uniq) ; do
     if [ -d $i ] ; then
         echo $i
         LLsub ./submit-test.sh -- --model_folder="$i" --model_file=best_model.pt --test_prefix=time_and_score
