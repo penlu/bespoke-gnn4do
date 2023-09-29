00a0b5e8460fd749fa5a86fd65a528a1da3d57d8
Eric Lu <penlume@gmail.com>
2023-09-29 04:50:27 -0400

    run-retest.sh

--- scripts/run-retest.sh
+++ scripts/run-retest.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for i in $(cat analysis_ipynb/models_for_test.txt | cut -d "'" -f 2 | sort | uniq) ; do
+for i in $(cat analysis_ipynb/models_for_test2.txt | cut -d "'" -f 2 | head -n 52) ; do
     echo $i
     LLsub ./submit-retest.sh -- $i
 done
