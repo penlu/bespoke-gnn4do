1ef1437e2abe1024a8522bcf3e39976a444a8a8d
Eric Lu <penlume@gmail.com>
2023-12-16 17:12:58 -0500

    update run-sat again

--- run-sat.sh
+++ run-sat.sh
@@ -11,7 +11,7 @@ for MODEL in 'LiftMP' 'GAT' 'GIN' 'GCNN' 'GatedGCNN' ; do
                   --stepwise=True --steps=100000 \
                   --valid_freq=2000 --dropout=0 \
                   --prefix=231216_sat \
-                  --model_type=$MODEL --dataset=$DATASET --infinite=True \
+                  --model_type=$MODEL --dataset=$DATASET --infinite=True --parallel=8 \
                   --num_layers=$LIFT_LAYERS --rank=$R \
                   --penalty=1 --problem_type=sat \
                   --batch_size=32 --gen_n=100 --gen_k=200
