c0da84e9e031f19fecad538d80992e3f558b8a07
Eric Lu <penlume@gmail.com>
2024-01-06 15:42:31 -0500

    change gset loader

--- data/gset.py
+++ data/gset.py
@@ -6,6 +6,7 @@ import networkx as nx
 import numpy as np
 import torch_geometric
 from torch_geometric.utils import from_networkx
+from torch_geometric.data import Data, Batch
 import torch
 import re
 
@@ -16,23 +17,21 @@ def load_mtx(path):
         weights = []
         first_line = True
         for line in f:
-            if not line[0] == '%':
-                s = line.split()
-                if first_line:
-                    first_line = False
-                else:
-                    g[0].append(int(s[0]) - 1, int(s[1]) - 1)
-                    g[1].append(int(s[1]) - 1, int(s[0]) - 1)
-                    if len(s) > 2:
-                        weights.append(int(s[2]))
-                        weights.append(int(s[2]))
-    if len(weights) < g.number_of_edges():
-        weights = np.ones(g.number_of_edges())
-    else:
-        print("WEIGHTED", path)
-        weights = np.int64(weights)
-    return g, weights
+            if line[0] == '%':
+                continue
 
+            if first_line:
+                first_line = False
+                continue
+
+            s = line.split()
+            g[0].append(int(s[0]) - 1, int(s[1]) - 1)
+            g[1].append(int(s[1]) - 1, int(s[0]) - 1)
+            if len(s) > 2:
+                weights.append(int(s[2]))
+                weights.append(int(s[2]))
+
+    return g, weights
 
 def load_gset(gset_path):
     #Prepping the dataset
@@ -42,29 +41,30 @@ def load_gset(gset_path):
     notgs = 0
     gs = []
     for file in listdir(gset_path):
-        tokens = file.split('.')    
-        if len(tokens)==2 and 'mtx' in tokens[1]:
-            counter+=1
-            g, weights = load_mtx(gset_path+ '/'+file)
-            graphs_and_weights[tokens[0]]=[g,weights]
+        tokens = file.split('.')
+        if len(tokens) == 2 and 'mtx' in tokens[1]:
+            counter += 1
+            edge_index, edge_weight = load_mtx(gset_path+ '/'+file)
+            graphs_and_weights[tokens[0]] = [edge_index, edge_weight]
+
         for token in tokens:
             if 'txt' in token:
-                with open(gset_path+ '/'+file, mode='r',  encoding='utf-8-sig') as f:
+                with open(gset_path+'/'+file, mode='r', encoding='utf-8-sig') as f:
                     lines = f.readlines()
                     for chunk in lines:
                         splitline = chunk.split('\t')
                         if not 'G' in chunk:
-                            notgs+=1
+                            notgs += 1
                             optimal = splitline[1].split('(')[0]
                             optimal = optimal.replace(".","")
                             optimal = optimal.replace(",","")
                             optimal_values+=[optimal]
                         else:
-                            notgs+=1
+                            notgs += 1
                             currg = splitline[0]
-                            gs+=[currg]
+                            gs += [currg]
                         parts = re.split('\n | \t', chunk)
-                        
+
     best_known_gs = {}
     for g,opt in zip(gs,optimal_values):
         best_known_gs[g]=opt
@@ -72,13 +72,16 @@ def load_gset(gset_path):
     for g in graphs_and_weights.keys():
         graphs_and_weights[g]+=[best_known_gs[g]]
 
+    # at this point, graphs_and_weights is a dict from names (e.g. "G14") to:
+    # a tuple of [edge_index, edge_weight], and possibly a third item representing optimal score
+
     pyg_dataset = []
     for graph in graphs_and_weights.keys():
-        pyg_graph = from_networkx(graphs_and_weights[graph][0])
-        pyg_graph.weights = torch.FloatTensor(graphs_and_weights[graph][1])
-        print(graphs_and_weights[graph][2])
-        pyg_graph.optimal = torch.FloatTensor(int(graphs_and_weights[graph][2]))
-        pyg_graph.name = graph
-        pyg_dataset+=[pyg_graph]
+        pyg_graph = Data(
+            edge_index=torch.LongTensor(graphs_and_weights[graph][0]),
+            edge_weight=torch.FloatTensor(graphs_and_weights[graph][1]),
+            optimal=torch.FloatTensor(int(graphs_and_weights[graph][2])),
+            name=graph)
+        pyg_dataset += [pyg_graph]
 
     return pyg_dataset
